9db73f8834cbf032e8fc4056fae37e090f236214
Aravind Suresh <aravind.2002.suresh@gmail.com>
2022-01-12 04:49:21 +0530

    Update arav-abi-queries.sql

--- sql-queries/arav-abi-queries.sql
+++ sql-queries/arav-abi-queries.sql
@@ -58,7 +58,7 @@ INSERT INTO Billing(units_consumed_cycle) VALUES('1650');
 INSERT INTO Billing(units_consumed_cycle) VALUES('2100');
 INSERT INTO Billing(units_consumed_cycle) VALUES('970');
 INSERT INTO Billing(units_consumed_cycle) VALUES('2250');
--- 7.	Energy_charge
+-- 7.	Energy_charge //BILLS
 CREATE TABLE Energy_charge(ec_id integer AUTO_INCREMENT , energy_charge_per_unit float, tariff_slab varchar(10), total_energy_charges float, CONSTRAINT PK_EC PRIMARY KEY(ec_id));
 
 INSERT INTO Energy_charge(energy_charge_per_unit , tariff_slab , total_energy_charges) VALUES('1200','2342','4366');
@@ -67,7 +67,7 @@ INSERT INTO Energy_charge(energy_charge_per_unit , tariff_slab , total_energy_ch
 INSERT INTO Energy_charge(energy_charge_per_unit , tariff_slab , total_energy_charges) VALUES('1908','2341','1234');
 INSERT INTO Energy_charge(energy_charge_per_unit , tariff_slab , total_energy_charges) VALUES('4312','1231','3421');
 
--- 8.	other_charge
+-- 8.	other_charge //BILLS
 CREATE TABLE other_charge(oc_id integer AUTO_INCREMENT, Duty float, GST float, fixed_charges float, total_other_charges float CONSTRAINT PK_EC PRIMARY KEY(oc_id));
 INSERT INTO other_charge(Duty, GST, fixed_charges, total_other_charges) VALUES('1212','5656','8988','5676');
 INSERT INTO other_charge(Duty, GST, fixed_charges, total_other_charges) VALUES('1122','3908','7771','3411');
@@ -79,25 +79,25 @@ INSERT INTO other_charge(Duty, GST, fixed_charges, total_other_charges) VALUES('
 
 
 -- 9.	Appliances
-CREATE TABLE Appliances(cons_appl_id integer AUTO_INCREMENT, office integer(5), laundry integer(5), Heating_Cooling integer(5), lights integer(5), kitchen integer(5), misc integer(5), tools integer(5) CONSTRAINT PK_EC PRIMARY KEY(cons_appl_id));
+CREATE TABLE Appliances(cons_appl_id integer(5), office integer(3), laundry integer(3), Heating_Cooling integer(3), Living_Room integer(3), lights integer(3), kitchen integer(3), misc integer(3), tools integer(3), CONSTRAINT PK_EC PRIMARY KEY(cons_appl_id));
 
-INSERT INTO Appliances( office , laundry  , Heating_Cooling, lights, kitchen, misc, tools) VALUES('23545','23423','32423','32435','23455','12300','23131'); 
-INSERT INTO Appliances( office , laundry  , Heating_Cooling, lights, kitchen, misc, tools) VALUES('24313','23413','23211','22111','11231','22113','11223'); 
-INSERT INTO Appliances( office , laundry  , Heating_Cooling, lights, kitchen, misc, tools) VALUES('11233','34521','334421','23232''13132','45321','12323'); 
-INSERT INTO Appliances( office , laundry  , Heating_Cooling, lights, kitchen, misc, tools) VALUES('23231','19867','18881','14341','10932','54123','12342'); 
-INSERT INTO Appliances( office , laundry  , Heating_Cooling, lights, kitchen, misc, tools) VALUES('11223','45421','90213','32412','32675','45451','23421'); 
-INSERT INTO Appliances( office , laundry  , Heating_Cooling, lights, kitchen, misc, tools) VALUES( ); 
+INSERT INTO Appliances VALUES(86490,10,20,15,23,12,18,6,9); 
+INSERT INTO Appliances VALUES(24313,23413,23211,22111,11231,22113,11223,11223,11223); 
+INSERT INTO Appliances VALUES(11233,34521,33442,22323,13132,45321,12323,12323,12323); 
+INSERT INTO Appliances VALUES(23231,19867,18881,14341,10932,54123,12342,12342,12342); 
+INSERT INTO Appliances VALUES(11223,45421,90213,32412,32675,45451,23421,23421,23421); 
+
 
 
 -- 10.	Consumption
-CREATE TABLE Consumption(cons_category_id integer AUTO_INCREMENT, units_consumed_heavy integer(5), units_consumed_medium integer(4), units_consumed_low integer(3) CONSTRAINT PK_EC PRIMARY KEY(cons_category_id));
+CREATE TABLE Consumption(cons_category_id integer, units_consumed_heavy integer(3), units_consumed_medium integer(3), units_consumed_low integer(3), CONSTRAINT PK_EC PRIMARY KEY(cons_category_id));
 
-INSERT INTO Consumption( units_consumed_heavy , units_consumed_medium  , units_consumed_low) VALUES('10400','2341','300'); 
-INSERT INTO Consumption( units_consumed_heavy , units_consumed_medium  , units_consumed_low) VALUES('20320','4441','123'); 
-INSERT INTO Consumption( units_consumed_heavy , units_consumed_medium  , units_consumed_low) VALUES('30333','2221','212'); 
-INSERT INTO Consumption( units_consumed_heavy , units_consumed_medium  , units_consumed_low) VALUES('23434','1231','120'); 
-INSERT INTO Consumption( units_consumed_heavy , units_consumed_medium  , units_consumed_low) VALUES('55785','7676','671'); 
-INSERT INTO Consumption( units_consumed_heavy , units_consumed_medium  , units_consumed_low) VALUES('20321','2345','567'); 
+INSERT INTO Consumption VALUES(86490,102,48,89);
+INSERT INTO Consumption VALUES(20320,4441,123,250); 
+INSERT INTO Consumption VALUES(30333,2221,212,250); 
+INSERT INTO Consumption VALUES(23434,1231,120,250); 
+INSERT INTO Consumption VALUES(55785,7676,671,250); 
+INSERT INTO Consumption VALUES(20321,2345,567,250); 
 
 
 
@@ -116,19 +116,19 @@ INSERT INTO Payment_details( transaction_id , pay_amt  , pay_status, pay_date, p
 
 
 
--- 12.	Bill_details
-CREATE TABLE Bill_details(bill_no varchar(10), user_id integer(6), transaction_id integer(10), bill_amt float, bill_area integer, issue_month varchar(9), issue_date date, CONSTRAINT PK_billDet PRIMARY KEY(bill_no, user_id), FOREIGN key(bill_no) REFERENCES Billing(bill_no), FOREIGN key(user_id) REFERENCES customer_details(user_id), FOREIGN KEY(transaction_id) REFERENCES Payment_details(transaction_id));
+-- 12.	Bill_details //BILLS
+CREATE TABLE Bill_details(bill_no varchar(10), user_id integer(6), transaction_id integer(10), bill_amt float, bill_area integer, issue_month varchar(9), issue_date date,due_date date,discon_date date, CONSTRAINT PK_billDet PRIMARY KEY(bill_no, user_id), FOREIGN key(bill_no) REFERENCES Billing(bill_no), FOREIGN key(user_id) REFERENCES customer_details(user_id), FOREIGN KEY(transaction_id) REFERENCES Payment_details(transaction_id));
 
 
-INSERT INTO Bill_details( transaction_id , bill_amt  , bill_area, issue_month, issue_date) VALUES("642583757212" ,4791 ,231348 ,"Jan" ,'20 Jan 21' ); 
-INSERT INTO Bill_details( transaction_id , bill_amt  , bill_area, issue_month, issue_date) VALUES( "569225387797" ,4326,611273 ,"Nov", '02 Nov 21' ); 
-INSERT INTO Bill_details( transaction_id , bill_amt  , bill_area, issue_month, issue_date) VALUES( "159320833161",4873  ,893958 ,"Jan" ,  '18 Jan 21'); 
-INSERT INTO Bill_details( transaction_id , bill_amt  , bill_area, issue_month, issue_date) VALUES("640961225729" , 7931,388011 , "Feb", '13 Feb 21' ); 
-INSERT INTO Bill_details( transaction_id , bill_amt  , bill_area, issue_month, issue_date) VALUES( "629331870828" ,6080,818357 ,"Dec"  ,'09 Dec 21'); 
-INSERT INTO Bill_details( transaction_id , bill_amt  , bill_area, issue_month, issue_date) VALUES("954297254243" ,7347  ,813010 ,"Oct" ,'01 Oct 21'); 
-INSERT INTO Bill_details( transaction_id , bill_amt  , bill_area, issue_month, issue_date) VALUES("936810891522" ,7347  ,813010 ,"Oct" ,'01 Oct 21'); 
-INSERT INTO Bill_details( transaction_id , bill_amt  , bill_area, issue_month, issue_date) VALUES("481215591710" ,7347  ,813010 ,"Oct" ,'01 Oct 21'); 
-INSERT INTO Bill_details( transaction_id , bill_amt  , bill_area, issue_month, issue_date) VALUES("446864205641" ,7347  ,813010 ,"Oct" ,'01 Oct 21'); 
+INSERT INTO Bill_details VALUES("642583757212",1234, 642583757212, 4791, 231348 ,'Jan', '20 Jan 21', '20 Jan 21', '20 Jan 21'); 
+INSERT INTO Bill_details VALUES("569225387797",1234, 569225387797, 4326, 611273 ,'Nov', '02 Nov 21', '02 Feb 21', '02 Feb 21'); 
+INSERT INTO Bill_details VALUES("159320833161",1234, 159320833161, 4873, 893958 ,'Jan', '18 Jan 21', '18 Mar 21', '18 Mar 21'); 
+INSERT INTO Bill_details VALUES("640961225729",1234, 640961225729, 7931, 388011 ,'Feb', '13 Feb 21', '13 Apr 21', '13 Apr 21'); 
+INSERT INTO Bill_details VALUES("629331870828",1234, 629331870828, 6080, 818357 ,'Dec', '09 Dec 21', '09 May 21', '09 May 21'); 
+INSERT INTO Bill_details VALUES("954297254243",1234, 954297254243, 7347, 813010 ,'Oct', '01 Oct 21', '01 Jun 21', '01 Jun 21'); 
+INSERT INTO Bill_details VALUES("936810891522",1234, 936810891522, 7347, 813010 ,'Oct', '01 Oct 21', '01 Jul 21', '01 Jul 21'); 
+INSERT INTO Bill_details VALUES("481215591710",1234, 481215591710, 7347, 813010 ,'Oct', '01 Oct 21', '01 Aug 21', '01 Aug 21'); 
+INSERT INTO Bill_details VALUES("446864205641",1234, 446864205641, 7347, 813010 ,'Oct', '01 Oct 21', '01 Sep 21', '01 Sep 21');
 
 
 
