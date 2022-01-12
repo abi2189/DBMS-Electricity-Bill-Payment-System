51b88d20bf5fe2f14bccd1d3b7f4458dc8e12d96
Abirami S <82747267+abi2189@users.noreply.github.com>
2022-01-12 12:26:24 +0530

    Update updated queries.sql

--- sql-queries/updated queries.sql
+++ sql-queries/updated queries.sql
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:sql-queries/updated queries.sql
 CREATE TABLE user_details(user_id varchar(6), first_name varchar(30), last_name varchar(30), DOB date, phone_no varchar(10), email_id varchar(30), passw varchar(20), CONSTRAINT PK_Customer PRIMARY KEY(user_id, phone_no));
 
 insert into user_details values('pat228','Arav','Abi','1989-01-20','9232348904','pat.johnston@example.com','jarhead');
@@ -5,6 +6,17 @@ insert into user_details values('lan348','Landon ','Morris','1992-02-12','902348
 insert into user_details values('gla343','Gladys ','Morris','1988-01-10','9232333444','gladys.morris@example.com','drpepper');
 insert into user_details values('lon612','Lonnie','Hopkins','1991-08-22','9129038904','lonnie.hopkins@example.com','justus');
 insert into user_details values('yvo809','Yvonne','Garrett','1985-12-31','9090808904','yvonne.garrett@example.com','mustang6');
+=======
+1.	user_details
+CREATE TABLE user_details(user_id integer(6), first_name varchar(30), last_name varchar(30), DOB date, phone_no integer(10), email_id varchar(30), passw varchar(20), CONSTRAINT PK_Customer PRIMARY KEY(user_id, phone_no));
+
+
+insert into user_details values('pat228','Arav','Abi','1985-01-05','9232348904','pat.johnston@example.com','jarhead');
+insert into user_details values('lan348','Landon ','Morris','1985-01-04','9023488904','landon.morris@example.com','homeboy');
+insert into user_details values('gla343','Gladys ','Morris','1986-12-05','9232333444','gladys.morris@example.com','drpepper');
+insert into user_details values('lon612','Lonnie','Hopkins','1977-11-03','9129038904','lonnie.hopkins@example.com','justus');
+insert into user_details values('yvo809','Yvonne','Garrett','1965-08-23','9090808904','yvonne.garrett@example.com','mustang6');
+>>>>>>> Stashed changes:sql-queries/updated queries.txt
 
 insert into user_details values('ari231','Arisa','Page','1985-01-05', '9055129336','ari.page@example.com','pages855');
 insert into user_details values('jes766','Jessie','McCoy','1986-10-08', '9043307087','jes.mcc@example.com','wadada12');
@@ -19,12 +31,17 @@ INSERT INTO customer_details VALUES('gla343', 1, 479001,'7503589801761989 ','Gla
 INSERT INTO customer_details VALUES('lon612', 0, 432098,'3057171446652151 ','Lonnie','2026-07-29',230);
 INSERT INTO customer_details VALUES('yvo809', 0, 324561,'8417146815012068 ','Yvonne','2023-06-20',560);
 
+<<<<<<< Updated upstream:sql-queries/updated queries.sql
+=======
+3.	Admin_details
+>>>>>>> Stashed changes:sql-queries/updated queries.txt
 CREATE TABLE Admin_details(admin_id varchar(6), user_id varchar(6), admin_control_area varchar(15), CONSTRAINT PK_Admin PRIMARY KEY(admin_id, user_id));
 insert into Admin_details values('ari231','pat228',560024); 
 insert into Admin_details values('jes766','lan348',560024);
 insert into Admin_details values('bea504','gla343',560024);
 insert into Admin_details values('aly872','lon612',231102); 
 insert into Admin_details values('sla772','yvo809',231102); 
+<<<<<<< Updated upstream:sql-queries/updated queries.sql
 CREATE TABLE Address(pincode integer, city varchar(20), state varchar(20), CONSTRAINT PK_Address PRIMARY KEY(pincode));
 
 insert into Address values(599118,'Bangalore','Karnataka');
@@ -32,6 +49,17 @@ insert into Address values(600001,'Chennai','Tamilnadu');
 insert into Address values(110001,'Delhi','Haryana');
 insert into Address values(230532,'Mumbai','Maharashtra');
 insert into Address values(560091,'Hyderabad','Andrapradesh');
+=======
+4.	Address
+CREATE TABLE Address(user_id varchar(6), pincode integer, city varchar(20), state varchar(20), CONSTRAINT PK_Address PRIMARY KEY(pincode));
+
+insert into Address values('pat228',599118,'Bangalore','Karnataka');
+insert into Address values('lan348',600001,'Chennai','Tamilnadu');
+insert into Address values('gla343',110001,'Delhi','Haryana');
+insert into Address values('lon612',230532,'Mumbai','Maharashtra');
+insert into Address values('yvo809',560091,'Hyderabad','Andrapradesh');
+5.	service_details
+>>>>>>> Stashed changes:sql-queries/updated queries.txt
 CREATE TABLE service_details(service_no integer(3), pincode integer(6), CONSTRAINT PK_ser PRIMARY KEY(service_no), FOREIGN key(pincode) REFERENCES Address(pincode), FOREIGN key(service_no) REFERENCES customer_details(service_no));
 INSERT INTO service_details VALUES(599,599118);
 INSERT INTO service_details VALUES(600,600001);
@@ -91,6 +119,7 @@ INSERT INTO Billing VALUES('746864205641', 55785, 11224, 224, 114, 2250);
 
 CREATE TABLE Payment_details(transaction_id varchar(10), pay_amt float, pay_status varchar(10), pay_date date, pay_time timestamp, CONSTRAINT PK_Payment PRIMARY KEY(transaction_id));
 
+<<<<<<< Updated upstream:sql-queries/updated queries.sql
 INSERT INTO Payment_details VALUES('8546509433',4791, 'Successful','2022-01-21','2021-01-20 23:30:23'); 
 INSERT INTO Payment_details VALUES('8485327687',4326, 'Successful', '2021-11-02','2021-11-02 14:48:33'); 
 INSERT INTO Payment_details VALUES('2757608823',4326, 'Failed', '2021-11-09','2021-09-02 14:48:33'); 
@@ -136,3 +165,11 @@ INSERT INTO Feedback VALUES(244 , 'gla343', 'Payment was pretty smooth','2021-01
 INSERT INTO Feedback VALUES(743 , 'lon612', 'Auto renewal not working','2021-02-13','2021-02-13 12:56:45'); 
 INSERT INTO Feedback VALUES(192 , 'yvo809', 'It is good','2021-12-09','2021-12-09 11:13:56'); 
 INSERT INTO Feedback VALUES(632 , 'yvo809', 'Auto renewal option is really helpful','2021-10-01','2021-10-01 17:00:45'); 
+=======
+INSERT INTO Feedback VALUES(127 , 'pat228', 'It was a really good user friendly website, made the paymenet process esier','2022-02-02','2021-01-20 23:30:23'); 
+INSERT INTO Feedback VALUES(929 , 'lan348', 'Had a problem with payment process','2021-11-02','2021-11-02 14:48:33'); 
+INSERT INTO Feedback VALUES(244 , 'gla343', 'Payment was pretty smooth','2021-11-02','2021-01-18 06:46:34'); 
+INSERT INTO Feedback VALUES(743 , 'lon612', 'Auto renewal not working','2021-11-02','2021-02-13 12:56:45'); 
+INSERT INTO Feedback VALUES(192 , 'yvo809', 'It is good',''2021-11-02','2021-12-09 11:13:56'); 
+INSERT INTO Feedback VALUES(632 , 'yvo809', 'Auto renewal option is really helpful','2021-11-02','2021-10-01 17:00:45'); 
+>>>>>>> Stashed changes:sql-queries/updated queries.txt
